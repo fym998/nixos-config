00063cd2663518bd18e535b3f7ca5c9c9749ccdc
fym998 <fujun998@outlook.com>
2025-06-09 10:36:05 +0800

    feat: rename matrix recovery key and update public keys in secrets.nix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,13 @@
 let
   fym_on_legion-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILYregZ1VAGwwCnFJzVmePI2CLsNaVTWzvCBghEEicgP fym@legion-nixos";
   root_on_legion-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINk0b1d4zHPhfV0je9t2mezyQttbWSKKC0i9T9ER0tRl root@legion-nixos";
+  redmi = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINvaO32WYshCazWrbxkkr1vqlWmgUsANmVq9nyX1ovHP u0_a377@localhost";
+  honor = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN+XwudZm9jiNkALdZSSKzUP9nNa8s8voq24/Eo6Q7lG u0_a200@localhost";
   users = [
     fym_on_legion-nixos
     root_on_legion-nixos
+    redmi
+    honor
   ];
 
   legion-nixos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM2buXuMxA+kXuEkspoNtoSTBCWyDTtGwMJtqlakB1hL root@nixos";
@@ -14,6 +18,9 @@ in
 {
   "mihomo-config.age".publicKeys = all;
   "password-root.age".publicKeys = [ root_on_legion-nixos ] ++ systems;
-  "password-fym.age".publicKeys = all;
-  "matrix-element-recovery-key.age".publicKeys = all;
+  "password-fym.age".publicKeys = [
+    fym_on_legion-nixos
+    root_on_legion-nixos
+  ] ++ systems;
+  "matrix-recovery-key.age".publicKeys = all;
 }
