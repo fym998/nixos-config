3d8cdba2a8a55ff6d022ee6770639a164ebde2da
fym998 <fujun998@outlook.com>
2025-06-14 11:27:37 +0800

    Add configuration files for fcitx5 input method
    - Created configuration files for various fcitx5 components including chttrans, cloudpinyin, notifications, pinyin, punctuation, and main config.
    - Defined settings for translation engines, input methods, hotkeys, and behavior preferences.
    - Established a profile with default input methods and layout settings.
    - Updated input-method.nix to include additional addons and source the new configuration files.

--- home-manager/input-method.nix
+++ home-manager/input-method.nix
@@ -6,32 +6,16 @@
     fcitx5 = {
       waylandFrontend = true;
       fcitx5-with-addons = pkgs.kdePackages.fcitx5-with-addons;
-      # plasma6Support = true;
-      addons = with pkgs; [ kdePackages.fcitx5-chinese-addons ];
-      settings = {
-        inputMethod = {
-          GroupOrder."0" = "Default";
-          "Groups/0" = {
-            Name = "Default";
-            "Default Layout" = "us";
-            DefaultIM = "pinyin";
-          };
-          "Groups/0/Items/0".Name = "keyboard-us";
-          "Groups/0/Items/1".Name = "pinyin";
-        };
-        addons.pinyin = {
-          globalSection = {
-            # 启用云拼音
-            CloudPinyinEnabled = "False";
-            # FirstRun
-            FirstRun = "False";
-          };
-          sections = {
-            PrevPage."0" = "bracketleft";
-            NextPage."0" = "bracketright";
-          };
-        };
-      };
+      addons = with pkgs; [
+        kdePackages.fcitx5-chinese-addons
+        fcitx5-mozc
+        fcitx5-pinyin-moegirl
+        fcitx5-pinyin-minecraft
+        fcitx5-pinyin-zhwiki
+      ];
     };
+  };
+  xdg.configFile = {
+    "fcitx5".source = ./files/config/fcitx5;
   };
 }
