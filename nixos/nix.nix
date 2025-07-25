c87464c5af07528d80f99fab6665e0e513ab24a5
fym998 <61316972+fym998@users.noreply.github.com>
2025-07-25 19:38:32 +0800

    enable nix auto gc and disable channel

--- nixos/nix.nix
+++ nixos/nix.nix
@@ -1,27 +1,34 @@
 {
-  nix.settings = rec {
-    cores = 13;
-    allow-import-from-derivation = false;
-    allowed-users = [ "@wheel" ];
-    trusted-users = allowed-users;
-    substituters = [
-      # "https://mirrors.cernet.edu.cn/nix-channels/store"
-      "https://mirror.sjtu.edu.cn/nix-channels/store"
-      "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-      "https://nix-community.cachix.org"
-      "https://fym998-nur.cachix.org"
-      "https://pre-commit-hooks.cachix.org"
-    ];
-    trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "fym998-nur.cachix.org-1:lWwztkEXGJsiJHh/5FbA2u95AxJu8/k4udgGqdFLhOU="
-      "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
-    ];
-    experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-    auto-optimise-store = true;
-    flake-registry = "";
+  nix = {
+    channel.enable = false;
+    gc = {
+      automatic = true;
+      dates = "weekly";
+    };
+    settings = rec {
+      cores = 13;
+      allow-import-from-derivation = false;
+      allowed-users = [ "@wheel" ];
+      trusted-users = allowed-users;
+      substituters = [
+        # "https://mirrors.cernet.edu.cn/nix-channels/store"
+        "https://mirror.sjtu.edu.cn/nix-channels/store"
+        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+        "https://nix-community.cachix.org"
+        "https://fym998-nur.cachix.org"
+        "https://pre-commit-hooks.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "fym998-nur.cachix.org-1:lWwztkEXGJsiJHh/5FbA2u95AxJu8/k4udgGqdFLhOU="
+        "pre-commit-hooks.cachix.org-1:Pkk3Panw5AW24TOv6kz3PvLhlH8puAsJTBbOPmBo7Rc="
+      ];
+      experimental-features = [
+        "nix-command"
+        "flakes"
+      ];
+      auto-optimise-store = true;
+      flake-registry = "";
+    };
   };
 }
