1c955e839b5ecd600cb2f860ae0518a08759b116
fym998 <61316972+fym998@users.noreply.github.com>
2025-08-02 18:17:58 +0800

    Add Windows boot entry

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -8,26 +8,36 @@
   boot.loader = {
     efi.canTouchEfiVariables = true;
 
-    systemd-boot = {
-      enable = true;
-      configurationLimit = 15;
+    systemd-boot =
+      let
+        other-esp = "HD0b";
+      in
+      {
+        enable = true;
+        configurationLimit = 15;
 
-      edk2-uefi-shell.enable = true;
+        edk2-uefi-shell.enable = true;
 
-      extraEntries = {
-        "grub-HD0b.conf" = ''
-          title    GRUB
-          efi      /efi/edk2-uefi-shell/shell.efi
-          options  -nointerrupt -nomap -noversion -exit HD0b:\EFI\GRUB\grubx64.efi
-          sort-key ${config.boot.loader.systemd-boot.edk2-uefi-shell.sortKey}
-        '';
-        "archlinux-HD0b.conf" = ''
-          title    Arch Linux
-          efi      /efi/edk2-uefi-shell/shell.efi
-          options  -nointerrupt -nomap -noversion -exit HD0b:\vmlinuz-linux root=UUID=de0720b6-fffa-4dfc-8f16-d7df8fd7ec20 rw rootflags=subvol=@  loglevel=4 initrd=\intel-ucode.img initrd=\initramfs-linux.img
-          sort-key a
-        '';
+        extraEntries = {
+          "grub-HD0b.conf" = ''
+            title    GRUB
+            efi      /efi/edk2-uefi-shell/shell.efi
+            options  -nointerrupt -nomap -noversion -exit ${other-esp}:\EFI\GRUB\grubx64.efi
+            sort-key a
+          '';
+          "archlinux-HD0b.conf" = ''
+            title    Arch Linux
+            efi      /efi/edk2-uefi-shell/shell.efi
+            options  -nointerrupt -nomap -noversion -exit ${other-esp}:\vmlinuz-linux root=UUID=de0720b6-fffa-4dfc-8f16-d7df8fd7ec20 rw rootflags=subvol=@  loglevel=4 initrd=\intel-ucode.img initrd=\initramfs-linux.img
+            sort-key a
+          '';
+        };
+
+        windows."windows" = {
+          title = "Windows";
+          efiDeviceHandle = other-esp;
+          sortKey = "a";
+        };
       };
-    };
   };
 }
