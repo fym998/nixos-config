437a90d0e8d02df1b85157d50b73896436566bd2
fym998 <61316972+fym998@users.noreply.github.com>
2025-08-14 14:24:46 +0800

    Remove sd-boot entry `Arch Linux`

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -25,12 +25,12 @@
             options  -nointerrupt -nomap -noversion -exit ${other-esp}:\EFI\GRUB\grubx64.efi
             sort-key a
           '';
-          "archlinux-HD0b.conf" = ''
-            title    Arch Linux
-            efi      /efi/edk2-uefi-shell/shell.efi
-            options  -nointerrupt -nomap -noversion -exit ${other-esp}:\vmlinuz-linux root=UUID=de0720b6-fffa-4dfc-8f16-d7df8fd7ec20 rw rootflags=subvol=@  loglevel=4 initrd=\intel-ucode.img initrd=\initramfs-linux.img
-            sort-key a
-          '';
+          # "archlinux-HD0b.conf" = ''
+          #   title    Arch Linux
+          #   efi      /efi/edk2-uefi-shell/shell.efi
+          #   options  -nointerrupt -nomap -noversion -exit ${other-esp}:\vmlinuz-linux root=UUID=de0720b6-fffa-4dfc-8f16-d7df8fd7ec20 rw rootflags=subvol=@  loglevel=4 initrd=\intel-ucode.img initrd=\initramfs-linux.img
+          #   sort-key a
+          # '';
         };
 
         windows."windows" = {
