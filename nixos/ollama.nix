5648b56d4eaa073c38cd017d998ed72ce498ede2
fym998 <61316972+fym998@users.noreply.github.com>
2025-08-23 11:12:49 +0800

    Add environment variables for ollama service

--- nixos/ollama.nix
+++ nixos/ollama.nix
@@ -3,6 +3,9 @@
     ollama = {
       enable = true;
       acceleration = "cuda";
+      environmentVariables = {
+        OLLAMA_ORIGINS = "*";
+      };
     };
 
     nextjs-ollama-llm-ui.enable = true;
